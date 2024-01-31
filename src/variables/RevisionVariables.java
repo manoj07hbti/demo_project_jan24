8ad0935791f2eac9d07f8daccbacd3c444115b5b
Manoj Kumar <manojkumar.pcf@gmail.com>
2024-01-31 21:42:36 +0530

    This is for demo commit

---
+++ src/variables/RevisionVariables.java
@@ -0,0 +1,11 @@
+package variables;
+
+public class RevisionVariables {
+
+    public static void main(String[] args) {
+
+        String name="I am git demo class ";
+        System.out.println("Printing : "+name);
+
+    }
+}
