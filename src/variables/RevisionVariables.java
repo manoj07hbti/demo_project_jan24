09bec63eae653fd7a1b4d6c6de41ba5dcd98a38b
Manoj Kumar <manojkumar.pcf@gmail.com>
2024-01-31 21:44:14 +0530

    This is second demo commit

--- src/variables/RevisionVariables.java
+++ src/variables/RevisionVariables.java
@@ -5,6 +5,8 @@ public class RevisionVariables {
     public static void main(String[] args) {
 
         String name="I am git demo class ";
+
+
         System.out.println("Printing : "+name);
 
     }
