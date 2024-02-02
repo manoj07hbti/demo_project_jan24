cf76e89990c6b871aefe2d219defc38be8ef7992
prafful <praffulg604@gmail.com>
2024-02-03 01:56:39 +0530

    manupulation

---
+++ src/assignment/Variable_manupulation.java
@@ -0,0 +1,26 @@
+package assignment;
+
+public class Variable_manupulation {
+    public static void main(String[] args) {
+        int a=7;
+        int b=9;
+
+        int add =a*b;
+        System.out.println(add);
+
+        double divide =(double) a/b;
+        System.out.println(divide);
+
+        int square = a*a;
+        System.out.println(square);
+
+        int cube = a*a*a;
+        System.out.println(cube);
+
+
+
+
+
+
+    }
+}
