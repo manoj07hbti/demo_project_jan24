a4a0f79e520529453f53ee041867c11ab2135d5d
prafful <praffulg604@gmail.com>
2024-02-02 15:38:46 +0530

    Assignment

---
+++ src/assignment/Ass_Class4.java
@@ -0,0 +1,12 @@
+package assignment;
+
+public class Ass_Class4 {
+    public static void main(String[] args) {
+        String qwe = "krishna";
+        int asd = 133;
+        double zxc = 6352350;
+
+        System.out.println(qwe + "\n" + asd + "\n" + zxc);
+
+    }
+}
---
+++ src/variables/New_class.java
@@ -0,0 +1,14 @@
+package variables;
+
+public class New_class { public static void main(String[] args) {
+    String name = "Raju";
+    int age = 27;
+    double salary = 70000.26;
+    String city = "Tundla";
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(salary);
+    System.out.println(city);
+}
+}
