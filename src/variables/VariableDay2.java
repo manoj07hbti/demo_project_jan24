bb9afb8275708221551a31f9d806029b00b90db5
Manoj Kumar <manojkumar.pcf@gmail.com>
2024-02-02 21:40:34 +0530

    Variable day 2

---
+++ src/variables/VariableDay2.java
@@ -0,0 +1,19 @@
+package variables;
+
+public class VariableDay2 {
+
+    public static void main(String[] args) {
+      //SYNTAX Datatype varName = value ;  // declaration + Assignment
+        String name="Java";
+        System.out.println("Printing name:"+name);
+
+        int age=21;
+        System.out.println("Printing Age: "+age);
+        double salary=34432.21;
+        System.out.println("Printing salary :"+salary);
+
+        System.out.println("Printing all variable: "+name + " Age : " +age+" Salary: "+salary);
+
+
+    }
+}
---
+++ src/variables/VariableManipulation.java
@@ -0,0 +1,27 @@
+package variables;
+
+public class VariableManipulation {
+
+    public static void main(String[] args) {
+
+        // Addition of two number
+        int a=4;
+        int b=7;
+
+        int sum=a+b;
+
+        System.out.println("Sum os a and b is :"+sum);
+
+        int sub=a-b;
+        System.out.println("Subtraction  os a and b is :"+sub);
+
+        int multi=a*b;
+        System.out.println("Printing multiplication "+multi);
+
+        double divide=(double) a/b; // 0.57
+
+        System.out.println("Division is: "+divide);
+
+
+    }
+}
