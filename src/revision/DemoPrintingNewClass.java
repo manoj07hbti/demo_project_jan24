fb8a9fc4b6c6de0de35c2184070065dc0e4e4c03
pkpankajmail <158242801+pkpankajmail@users.noreply.github.com>
2024-02-02 21:19:21 +0530

    hii

---
+++ src/revision/DemoPrintingNewClass.java
@@ -0,0 +1,9 @@
+package revision;
+
+public class DemoPrintingNewClass {
+    public static void main(String[] args) {
+        String name;
+        name="Revision java";
+        System.out.println("Pringting name: "+name);
+    }
+}
