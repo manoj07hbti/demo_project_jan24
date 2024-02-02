5cdfb55483902da1d021cfabad444fb01353bb80
prafful <praffulg604@gmail.com>
2024-02-03 01:54:54 +0530

    manupulation

--- src/assignment/Ass_Class1.java
+++ src/assignment/Ass_Class1.java
@@ -5,6 +5,8 @@ public class Ass_Class1 {
         String name = "Prashant";
         int clas = 2;
         double marks = 98562.54;
-        System.out.println(name + "\n" + clas + "\n" + marks);
+        System.out.println(name+"\n"+clas+"\n" + marks);
+
+
     }
 }
