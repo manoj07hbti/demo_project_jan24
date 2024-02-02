a1df42777b4f121943a04204b8056bef25e065b3
prafful <praffulg604@gmail.com>
2024-02-02 21:12:00 +0530

    Assignment

--- src/assignment/Ass_Class1.java
+++ src/assignment/Ass_Class1.java
@@ -6,8 +6,5 @@ public class Ass_Class1 {
         int clas = 2;
         double marks = 98562.54;
         System.out.println(name + "\n" + clas + "\n" + marks);
-           // doing more changes for test
-        //JHJUGKJHB
-
     }
 }
