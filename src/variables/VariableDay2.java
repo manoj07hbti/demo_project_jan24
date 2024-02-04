90d25a857007a71ce6f0377ca5cc3d102862419a
pkpankajmail <158242801+pkpankajmail@users.noreply.github.com>
2024-02-04 14:36:41 +0530

    variable day two practice

---
+++ src/variables/VariableDay2.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class VariableDay2 {
+    public static void main(String[] args) {
+        //SYNTAX Datatype varName = value; // declaration + Assigment
+        String name="Java Hub";
+        System.out.println("Printing name:"+name);
+
+         int age=55;
+        System.out.println("Printing Age: "+age);
+
+        double salary=7645.09;
+        System.out.println("Printing salary:"+salary);
+        System.out.println("Printing all variable: "+name +" age :"+age+"Salary: "+salary);
+    }
+}
---
+++ src/variables/VariableManipulation.java
@@ -0,0 +1,26 @@
+package variables;
+
+public class VariableManipulation {
+    public static void main(String[] args) {
+
+        //Addition of two number
+
+        int a=7;
+        int b=9;
+        int sum=a+b;
+        System.out.println("Sum of a and b is :"+sum);
+
+
+
+        int sub=a-b;
+        System.out.println("subtraction of a and b is :"+sub);
+
+        int multi=a*b;
+        System.out.println("Printing multiplication :"+multi);
+
+
+        double divide=(double)a/b;  //  0.77
+        System.out.println("Printing Division :"+divide);
+
+    }
+}
