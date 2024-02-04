85f2405fe1d8691d48098dfadc79813ea58ed606
pkpankajmail <158242801+pkpankajmail@users.noreply.github.com>
2024-02-05 00:06:10 +0530

    variable day two practice

---
+++ src/variables/VariableDayTwo.java
@@ -0,0 +1,26 @@
+package variables;
+
+public class VariableDayTwo {
+    public static void main(String[] args) {
+
+        int number=3;
+        int square=number*number;
+        System.out.println("this is square number ;"+square);
+
+
+
+        int number1=4;
+        int square1=number1*number1;
+        System.out.println("this is second square number ;"+square1);
+
+
+        int number2=5;
+        int square2=number2*number2;
+        System.out.println("this is third square number ;"+square2);
+
+
+        int number3=6;
+        int square3=number3*number3;
+        System.out.println("this is four square number ; "+square3);
+    }
+}
