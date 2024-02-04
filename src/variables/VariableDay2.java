6e65948464ea71362f6a49e254875c3d3a5d631f
Kapoor27 <122999145+Kapoor27@users.noreply.github.com>
2024-02-04 20:45:16 -0300

    Variable Manipulation Revision

---
+++ src/variables/VariableDay2.java
@@ -0,0 +1,21 @@
+package variables;
+
+public class VariableDay2 {
+    public static void main(String[] args) {
+
+        //SYNTAX Datatype varName = value;
+        String name="Bittoo";
+        System.out.println("Printing name :"+name);
+        int age=36;
+        System.out.println("Printing age :"+age);
+        double salary=98765.90;
+        System.out.println("Printing salary :"+salary);
+
+        System.out.println("Printing all variable name: "+name + " Age :" +age+" salary :"+salary);
+
+
+
+    }
+
+
+}
---
+++ src/variables/VariableManipulation.java
@@ -0,0 +1,28 @@
+package variables;
+
+public class VariableManipulation {
+
+    public static void main(String[] args) {
+
+        // Addition of two number
+        int a=5;
+        int b=9;
+
+        int sum=a+b;
+
+        System.out.println("Sub of a and b is :"+sum);
+
+        int sub=a-b;
+        System.out.println("Subtraction of a and b is :"+sub);
+
+        int multi=a*b;
+        System.out.println("Multiplication of a and b is :"+multi);
+
+        double divide=(double)a/b;
+        System.out.println("divide of a and b is :"+divide);
+
+    }
+
+
+
+}
