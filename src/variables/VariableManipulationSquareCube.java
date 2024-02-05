bf0a02bb3be45203ea420817b9121d5ebc46d05d
Kapoor27 <122999145+Kapoor27@users.noreply.github.com>
2024-02-04 21:28:11 -0300

    Manipulation Of Square And Cube

---
+++ src/variables/VariableManipulationSquareCube.java
@@ -0,0 +1,22 @@
+package variables;
+
+public class VariableManipulationSquareCube {
+
+    public static void main(String[] args) {
+
+        // if A= 6 // what is the square value of A;
+        int A=6;
+        int square=A*A;
+        System.out.println("square of A is : "+square);
+
+        // if B= 9 // what is the cube value of B;
+        int B=9;
+        int cube=B*B*B;
+        System.out.println("cube of B is : "+cube);
+
+
+
+
+    }
+
+}
