12698fa07c0d64b9d4e0d608e3c447a2a012d8da
prafful <praffulg604@gmail.com>
2024-02-06 00:51:58 +0530

    Operators 1.0

---
+++ src/assignment/Operators.java
@@ -0,0 +1,12 @@
+package assignment;
+
+public class Operators {
+    public static void main(String[] args) {
+        int p = 7;
+        int o = 10;
+        p++;
+        o--;
+        System.out.println( p+"\n" +o);
+
+    }
+}
