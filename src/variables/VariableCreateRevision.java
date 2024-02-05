adadb901dbbe2ad74d6b0ae286efa7e62e02e15b
Kapoor27 <122999145+Kapoor27@users.noreply.github.com>
2024-02-04 22:04:58 -0300

    Manipulation 8 Variable Print all In One Line

---
+++ src/variables/VariableCreateRevision.java
@@ -0,0 +1,28 @@
+package variables;
+
+public class VariableCreateRevision {
+
+    public static void main(String[] args) {
+
+        // Variable Creation;
+        int a=2;
+        int b=4;
+        String name;
+        name= "Rajeev Kapoor";
+        String FatherName;
+        FatherName="Shri kailash Chandra";
+        int age=36;
+        double salary=98765.23;
+        int sum=a+b;
+        int sub=a-b;
+        int multiply=a*b;
+        double divide=(double)a/b;
+        System.out.println("Printing all variable name : "+ name + " FatherName :" + FatherName + " Age :" + age + " salary :"+ salary +" sum :"+ sum +" sub :"+ sub +" multiply :"+ multiply +" divide :"+ divide);
+
+
+
+
+    }
+
+
+}
