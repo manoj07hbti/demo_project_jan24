4b9020323411b0d02310291fd17cc53a07932ee1
pkpankajmail <158242801+pkpankajmail@users.noreply.github.com>
2024-02-05 20:38:22 +0530

    this is squre variable commit....!!

--- src/variables/VariableDayTwo.java
+++ src/variables/VariableDayTwo.java
@@ -7,21 +7,25 @@ public class VariableDayTwo {
         int square=number*number;
         System.out.println("this is square number ;"+square);
 
-
-
         int number1=4;
         int square1=number1*number1;
         System.out.println("this is second square number ;"+square1);
-
 
         int number2=5;
         int square2=number2*number2;
         System.out.println("this is third square number ;"+square2);
 
-
         int number3=6;
         int square3=number3*number3;
         System.out.println("this is four square number ; "+square3);
+
+        int number4=9;
+        int square4=number4*number4;
+        System.out.println("this is four square number ; "+square4);
+
+        int number5=16;
+        int square5=number5*number5;
+        System.out.println("this is four square number ; "+square5);
 
 
 
