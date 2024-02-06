1afa5e2168320500522e9e7df9126d3a0a0ab2a1
Kapoor27 <122999145+Kapoor27@users.noreply.github.com>
2024-02-05 21:00:35 -0300

    VariableCreateRevision2

---
+++ src/variables/VariableCreateRevision2.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class VariableCreateRevision2 {
+
+    public static void main(String[] args) {
+
+        //SYNTAX Datatype varName = value ; // declaration + Assignment
+        String name="Java Practice";
+        int age=32;
+        double salary=98765.45;
+        System.out.println("Printing name :"+ name + " Age :" + age + " salary :" + salary);
+
+    }
+
+
+}
