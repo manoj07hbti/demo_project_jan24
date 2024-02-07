ad4b0cacb1fdd3b898eaacc9e37c02199d3403c0
Idd mohammad <mohammadidd345@gmail.com>
2024-02-07 14:38:14 +0530

    Day 7 Percentage_Calculating

---
+++ src/Arithmetic_Operators/Percentage_Calculating.java
@@ -0,0 +1,14 @@
+package Arithmetic_Operators;
+
+public class Percentage_Calculating {
+    public static void main(String[] args) {
+
+        double i=34667;
+        double j=7;
+
+        double c = ((j/100)*i);
+
+        System.out.println(j+" Percentage of "+i+" is "+(c)+"%");
+
+    }
+}
