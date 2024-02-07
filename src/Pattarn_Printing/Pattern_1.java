5c47b2ef073806b1168f32b1adce9495b0593d2f
Idd mohammad <mohammadidd345@gmail.com>
2024-02-07 20:31:40 +0530

    making pyramid with stars

---
+++ src/Pattarn_Printing/Pattern_1.java
@@ -0,0 +1,18 @@
+package Pattarn_Printing;
+
+public class Pattern_1 {
+    public static void main(String[] args) {
+
+        for (int i=1;i<=5;i++){
+
+            for (int j=1;j<=5;j++){
+
+                if (i >=j ) {
+                    System.out.print("");
+                  //  System.out.println("\u2764");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
