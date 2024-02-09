f040c0f8a352e5e014efd7f77f94eacd14d0e422
Niranjan singh <ns201890@gmail.com>
2024-02-09 20:16:17 +0530

    if-condition

---
+++ src/my_conditional_statement/DemoIf.java
@@ -0,0 +1,11 @@
+package my_conditional_statement;
+
+public class DemoIf {
+    public static void main(String[] args) {
+        int number=60;
+        if(number>=60){
+            System.out.println("Student got first division-"+number);
+        }
+    }
+
+}
