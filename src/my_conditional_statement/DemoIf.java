92c9a8c8a03e7435b22508a4433d584258e00ccf
Niranjan singh <ns201890@gmail.com>
2024-02-09 20:41:29 +0530

    if-condition2

--- src/my_conditional_statement/DemoIf.java
+++ src/my_conditional_statement/DemoIf.java
@@ -6,6 +6,18 @@ public class DemoIf {
         if(number>=60){
             System.out.println("Student got first division-"+number);
         }
+        int number1=33;
+        if(number1<=33){
+            System.out.println("Student will not cleared the exam-"+number1);
+        }
+        String country="India";
+        if(country=="India"){
+            System.out.println("you are Indian-"+country);
+        }
+        String countryname="Uk";
+        if(countryname!="Indian"){
+            System.out.println("you are not a Indian-"+countryname);
+        }
     }
 
 }
