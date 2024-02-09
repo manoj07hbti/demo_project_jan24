0551507f10573ff3661244086c1a528c88adf018
Idd mohammad <mohammadidd345@gmail.com>
2024-02-09 15:38:34 +0530

    making pyramid with stars 2

---
+++ src/conditional_statements/if_statements/Only_If.java
@@ -0,0 +1,15 @@
+package conditional_statements.if_statements;
+
+public class Only_If {
+    public static void main(String[] args) {
+
+        int marks = 60;
+
+        if ( marks >= 60 ){
+            System.out.println("First Devision ");
+        }
+        if (marks < 33 ) {
+            System.out.println("Not Cleared Exam");
+        }
+    }
+}
---
+++ src/conditional_statements/if_statements/Price.java
@@ -0,0 +1,15 @@
+package conditional_statements.if_statements;
+
+public class Price {
+    public static void main(String[]args){
+
+        int price = 4000 ;
+
+        if ( price >= 5000 ){
+            System.out.println("Costly Product");
+        }
+        if (price < 5000 ){
+            System.out.println("Average Cost Product ");
+        }
+    }
+}
