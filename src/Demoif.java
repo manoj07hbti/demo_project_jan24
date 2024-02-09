8743c2a13e8ea38c4d382329f14b1aeb13ca858c
Zameer Raza <razazameer905@gmail.com>
2024-02-09 20:44:54 +0530

    assignment if

---
+++ src/Demoif.java
@@ -0,0 +1,12 @@
+public class Demoif {
+    public static void main(String[] args) {
+        int num = 72;
+        if (num >= 60) {
+            System.out.println("student passed with first division");
+        }
+        if (num < 33) {
+            System.out.println("student did not cleared the exam");
+        }
+
+    }
+}
