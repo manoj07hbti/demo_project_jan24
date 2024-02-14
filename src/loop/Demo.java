a1f0a3ed75c30181e6ba8ce34b6d47ec8c8c43c1
Manoj Kumar <manojkumar.pcf@gmail.com>
2024-02-14 21:55:39 +0530

    Loops Working Day1

---
+++ src/loop/Demo.java
@@ -0,0 +1,28 @@
+package loop;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+     /*   SYNTAX: for (start_condition; conditional_expression; increment){
+                      //CODE
+                        }                        */
+
+        for(int i=1; i<=5; i++){
+
+            System.out.println("Hello World..."+i);
+        }
+/*
+        SYNTAX: for (start_condition; conditional_expression; decrement){
+            //CODE
+        }*/
+
+        for(int j=10; j>0; j--){
+
+            System.out.println("Decrement for loop ...."+j);
+        }
+
+
+
+    }
+}
---
+++ src/loop/TableOfTwo.java
@@ -0,0 +1,15 @@
+package loop;
+
+public class TableOfTwo {
+    public static void main(String[] args) {
+
+        int number=2;
+
+        for(int i=1; i<=10; i++){
+
+            System.out.println(number +" X "+i+" = "+number*i);
+        }
+
+
+    }
+}
