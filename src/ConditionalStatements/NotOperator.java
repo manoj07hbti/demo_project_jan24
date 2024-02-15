7cbdaa71b527aacf8cfba9db94ed535013312604
Prafful <158169672+Prafful-g@users.noreply.github.com>
2024-02-15 15:57:48 +0530

    cleaning mess

--- src/ConditionalStatements/NotOperator.java
+++ src/ConditionalStatements/NotOperator.java
@@ -7,27 +7,25 @@ public class NotOperator {
         //TODO Write a code to check Country should not be indian
 
         String country = "india";
-        if (country != "india") {
-            System.out.println("Welcome");
-        } else {
-            System.out.println("User Not Allowed");
+        if (!(country != "india")) {
+            System.out.println("Not allowed");
         }
 
         //TODO write a code to check number is divisible by 5 using ! operator
 
-        int num = 565341;
-        if (num % 5 != 0) {
-            System.out.println("num not divisible by 5");
-        } else System.out.println("num is divisible by 5");
+        int num = 565;
+        if (!(num % 5 != 0)) {
+            System.out.println("num divisible by 5");
+        }
 
 
         //TODO write a code to check number is even or odd using ! operator
 
-        int n = 8;
-        if (n % 2 != 0) {
-            System.out.println("num is odd");
-        } else {
+        int n =5;
+        if (!(n % 2 != 0)) {
             System.out.println("num is even");
+        } else {
+            System.out.println("num is odd");
         }
     }
 }
