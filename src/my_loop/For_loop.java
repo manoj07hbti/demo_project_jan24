4600754a7123f17256c17a064ffc7ba76738f999
Niranjan singh <ns201890@gmail.com>
2024-02-15 20:18:51 +0530

    for_loop

---
+++ src/my_loop/For_loop.java
@@ -0,0 +1,16 @@
+package my_loop;
+
+public class For_loop {
+    public static void main(String[] args) {
+        //table of 5 using for loop
+        int number = 5;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("5 x " + i + " = " + number * i);
+        }
+        //table of 10 using for loop
+        int num = 10;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("10 x " + i + " = " + num * i);
+        }
+    }
+}
