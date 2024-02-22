8a714475939e71749a8c388df964ed8206b928d6
Niranjan singh <ns201890@gmail.com>
2024-02-22 19:44:18 +0530

    My_Array

---
+++ src/my_loop/MyArray.java
@@ -0,0 +1,25 @@
+package my_loop;
+
+public class MyArray {
+    public static void main(String[] args) {
+        String country1="India";
+        String country2="usa";
+        String country3="Africa";
+        String country4="Australia";
+        String country5="England";
+        //Datatype varName [] = {value1, value2,value3.......value N};
+        String countries []={"India","usa","Africa","Australia","England"};
+
+// printing array element based on index  varName[INDEX];
+
+        System.out.println("Printing data: "+ countries[0]);
+
+        // printing all element of array
+
+        for (int i=0; i<countries.length; i++){
+
+            System.out.println("Printing with loop: "+countries[i]);
+        }
+    }
+}
+
