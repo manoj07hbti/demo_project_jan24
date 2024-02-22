922e7583442d974d9bad4798443811ae88363ba4
Niranjan singh <ns201890@gmail.com>
2024-02-22 19:57:04 +0530

    Break_In_Loop

---
+++ src/my_loop/BreakInLoop.java
@@ -0,0 +1,26 @@
+package my_loop;
+
+public class BreakInLoop {
+    public static void main(String[] args) {
+
+        int marks []={40,50,60,45,65,38,46,78,33,78,54,66,88};
+        for(int var:marks){
+            System.out.println("Printing in advance loop :"+var);
+            if(var==33){
+                break;
+            }
+        }
+        String months []={"january","february","march","april","may","june","july","august","september","october","number","december"};
+        for(String var:months){
+            System.out.println("Printing in advance loop :"+var);
+            if(var=="december"){
+                break;
+            }
+        }
+
+
+    }
+
+    }
+
+
