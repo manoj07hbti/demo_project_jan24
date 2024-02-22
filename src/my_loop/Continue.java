1035f26184ab3138351d9612bda26ea2b42601fe
Niranjan singh <ns201890@gmail.com>
2024-02-22 20:01:40 +0530

    continue

---
+++ src/my_loop/Continue.java
@@ -0,0 +1,14 @@
+package my_loop;
+
+public class Continue {
+    public static void main(String[] args) {
+        String language[] = {"Java", "Python", "C", "C++", "Dotnet", "HTML", "Gherkin", "Golang", "VB"};
+        for (String var : language) {
+
+            if (var.equals("HTML")) {
+                continue;
+            }
+            System.out.println("Language is backend language:- " + var);
+        }
+    }
+}
