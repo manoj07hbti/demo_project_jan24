6ab44e7257284f00ccc543f66ec3f66a89878f25
owner <pkpankajemail@gmail.com>
2024-02-22 16:29:01 -0800

    this is first commit revisin

---
+++ src/revision/RevisionVariables.java
@@ -0,0 +1,10 @@
+package revision;
+
+public class RevisionVariables {
+    public static void main(String[] args) {
+
+        String name=" I am Java programmer ";
+
+        System.out.println("Printing adding more comments :  "+name);
+    }
+}
