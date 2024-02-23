424867695ef67a6c77cb85fda6059db1ea17251e
owner <pkpankajemail@gmail.com>
2024-02-22 21:21:39 -0800

    print variable single line

---
+++ src/variables/VariableSingleLine.java
@@ -0,0 +1,29 @@
+package variables;
+
+public class VariableSingleLine {
+    public static void main(String[] args) {
+
+        //print int variable in single line........
+
+
+        int a=1;
+        int b=2;
+        int c=3;
+        int d=4;
+        int e=5;
+        int f=6;
+        int g=7;
+        int h=8;
+        int i=9;
+        int j=10;
+        int k=11;
+
+
+
+
+        System.out.println(a+""+b+""+c+""+d+""+e+""+f+""+g+""+h+""+i+""+j+""+k);
+
+
+
+    }
+}
