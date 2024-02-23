0a7bf075f4a81fe8ba5b215e951e6fb655c5c920
owner <pkpankajemail@gmail.com>
2024-02-23 07:42:32 -0800

    this is first commit .....

---
+++ src/variables/VariableModulusOperator.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class VariableModulusOperator {
+    public static void main(String[] args) {
+
+        // modulus operator....
+
+        int a=427;
+        int b=9;
+
+
+        double per=(double) a*b/100;  // no*%/100
+        System.out.println("Printing calculation percentage :"+per);
+    }
+}
