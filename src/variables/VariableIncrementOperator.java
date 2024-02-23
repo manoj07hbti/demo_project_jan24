6cc876abaed47ebbac85d719af8be896fe1bc1cd
owner <pkpankajemail@gmail.com>
2024-02-23 08:01:07 -0800

    this is first commit .....

---
+++ src/variables/VariableIncrementOperator.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class VariableIncrementOperator {
+    public static void main(String[] args) {
+
+        // Post increment Operator variable++ =variable+1
+
+        int Pankaj=7;
+        System.out.println("Printing Pankaj before ++ Operator : "+Pankaj);
+
+        Pankaj++;  // Post increment
+
+        int Singh=9;
+        Singh--;
+        System.out.println("Printing Singh :"+Singh);
+    }
+}
---
+++ src/variables/VariablePyramid.java
@@ -0,0 +1,18 @@
+package variables;
+
+public class VariablePyramid {
+    public static void main(String[] args) {
+
+        String Pyramid="Pyramid";
+
+        System.out.println("Printing Pyarmid Patteren");
+        System.out.println("*"+"\n"+"**"+"\n"+"***"+"\n"+"****"+"\n"+"*****"+"\n"+"******");
+
+
+        System.out.println("Printing Reverse Pyramid ");
+        System.out.println("********"+"\n"+" *******"+"\n"+"  *****"+"\n"+"   ***"+"\n"+"    **"
+        );
+
+
+    }
+}
