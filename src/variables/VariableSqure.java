2596c0fa4235e4e1d6e9a6149b75dffe1768c303
owner <pkpankajemail@gmail.com>
2024-02-22 21:15:46 -0800

    variable square update....

---
+++ src/variables/VariableSqure.java
@@ -0,0 +1,30 @@
+package variables;
+
+public class VariableSqure {
+    public static void main(String[] args) {
+
+        int number1=3;
+        int square1=number1*number1;
+        System.out.println("this is cube number;"+square1);
+
+        int number2=9;
+        int square2=number2*number2;
+        System.out.println("this is cube number;"+square2);
+
+        int number3=8;
+        int square3=number3*number3;
+        System.out.println("this is cube number;"+square3);
+
+        int number4=17;
+        int square4=number4*number4;
+        System.out.println("this is cube number;"+square4);
+
+        int number5=21;
+        int square5=number5*number5;
+        System.out.println("this is cube number;"+square5);
+
+        int number6=32;
+        int square6=number6*number6;
+        System.out.println("this is cube number;"+square6);
+    }
+}
