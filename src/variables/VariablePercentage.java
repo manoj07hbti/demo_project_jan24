f742eb80e7f425cdcdfb29b55adc4f1fffb7c46e
owner <pkpankajemail@gmail.com>
2024-02-22 23:38:58 -0800

    this is first commit revisin

---
+++ src/variables/VariablePercentage.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class VariablePercentage {
+    public static void main(String[] args) {
+
+        //calculate percentage  of given number
+
+
+
+        int a=5742;
+        int b=12;
+
+        double per=(double)a*b/100; // number * percentage/100
+        System.out.println("Printing Calculation Percentage :"+per);
+    }
+}
