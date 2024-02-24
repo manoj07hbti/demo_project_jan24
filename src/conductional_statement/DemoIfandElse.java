02f62da5984ec2c14076c2c88bec986870bc4474
owner <pkpankajemail@gmail.com>
2024-02-23 16:36:39 -0800

    if and else commit

---
+++ src/conductional_statement/DemoIfandElse.java
@@ -0,0 +1,21 @@
+package conductional_statement;
+
+public class DemoIfandElse {
+    public static void main(String[] args) {
+
+
+        //TODO - Write a code to check given number positive number or not
+
+
+        int number = 3;
+        if (number >= 0) {
+            System.out.println("number is positive number :" + number);
+        } else {
+            System.out.println("number is negative number :" + number);
+
+
+        }
+    }
+}
+
+
---
+++ src/conductional_statement/DemoNastedIf.java
@@ -0,0 +1,16 @@
+package conductional_statement;
+
+public class DemoNastedIf {
+    public static void main(String[] args) {
+
+        int number=5;
+        if(number==0) {
+            System.out.printf("Number greater than Zero and Divisible by 5");
+        }else {
+            System.out.println("Number is grater than zero but not divisible by 5");
+
+        }
+
+    }
+}
+
