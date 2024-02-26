8923e16dc9430931c0e4c899929aab1d337e9d46
owner <pkpankajemail@gmail.com>
2024-02-26 13:12:09 -0800

    Loop table no 5 and 10 commit.....

---
+++ src/loop/TableFive.java
@@ -0,0 +1,20 @@
+package loop;
+
+public class TableFive {
+    public static void main(String[] args) {
+
+        //TODO create a table NO of 5
+
+
+        int number = 5;
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println(number + " X " + i + " = " + number * i);
+        }
+    }
+}
+
+
+
+
+
---
+++ src/loop/TableTen.java
@@ -0,0 +1,13 @@
+package loop;
+
+public class TableTen {
+    public static void main(String[] args) {
+
+
+        int number = 10;
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println(number + " X " + i + " = " + number * i);
+        }
+    }
+}
