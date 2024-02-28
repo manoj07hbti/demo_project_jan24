96eef231924b90a51d6459d09ebfa87d32e1b1f7
owner <pkpankajemail@gmail.com>
2024-02-28 14:44:38 -0800

    increment and decrement loops commit...

---
+++ src/loop/DecrementLoop.java
@@ -0,0 +1,16 @@
+package loop;
+
+public class DecrementLoop {
+    public static void main(String[] args) {
+
+        //* SYNTAX : for (start_ condition; conditional_expression; decrement){
+        // CODE  }*/
+
+
+        for(int y=99; y>0; y--){
+            System.out.println("Wellcome to my Java World..."+y);
+        }
+
+
+    }
+}
---
+++ src/loop/IncrementLoop.java
@@ -0,0 +1,28 @@
+package loop;
+
+public class IncrementLoop {
+    public static void main(String[] args) {
+        /* SYNTAX : for (start_ condition; conditional_expression; increment){
+        //CODE     }
+
+        */
+
+
+        for(int x=1; x<=15; x++){
+            System.out.println(" wellcome to my java world...."+x);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
