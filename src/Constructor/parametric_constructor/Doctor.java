a51450bc8c61c5f50b0edfda868482db770d852d
Idd mohammad <mohammadidd345@gmail.com>
2024-03-01 20:48:06 +0530

    Parametric Constructor just little change 01/03/2024

--- src/Constructor/parametric_constructor/Doctor.java
+++ src/Constructor/parametric_constructor/Doctor.java
@@ -8,15 +8,15 @@ public class Doctor {
     String Branch;
 
     public Doctor(int id, String name, double height, String specialist, String branch) {
-        Id = id;
+       this.Id = id;
         this.name = name;
         this.height = height;
-        Specialist = specialist;
-        Branch = branch;
+        this.Specialist = specialist;
+        this.Branch = branch;
     }
 
     public static void main(String[] args) {
-        Doctor doctor = new Doctor(1,"Umer",5.9,"Surgeon","Cardiologist");
+        Doctor doctor = new Doctor(1,"Umar",5.9,"Surgeon","Cardiologist");
 
         System.out.println("====Doctor Details===="+"\nName        : "+doctor.name+"\nID No.      : "+doctor.Id+
                 "\nHeight      : "+doctor.height+"\nSpecialist  : "+doctor.Specialist+"\nBranch      : "+doctor.Branch+"\n");
