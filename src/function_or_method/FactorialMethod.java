5091609faf378d3d484e7cfed867efd32edbaf67
owner <pkpankajemail@gmail.com>
2024-03-09 01:32:42 -0800

    CalculatorApplication , StudentDivision, FactorialMethod,and all Pending assignment function and method

--- src/function_or_method/FactorialMethod.java
+++ src/function_or_method/FactorialMethod.java
@@ -4,15 +4,15 @@ public class FactorialMethod {
     public void fac(int a, int b) {
 
 
-        for (int c = 1; c <= a; c++) {
-            b = b * c;
+        for (int c=1;   c<=a;   c++) {
+            b=b*c;
         }
-        System.out.println("We Are Printing Factorial Of a is :" + b);
 
+        System.out.println("Printing fectorial of A is :" + b);
     }
 
     public static void main(String[] args) {
         FactorialMethod obj = new FactorialMethod();
-        obj.fac(4, 1);
+        obj.fac(4,1);
     }
 }
---
+++ src/function_or_method/ReturnTypeCalculator.java
@@ -0,0 +1,6 @@
+package function_or_method;public class ReturnTypeCalculator {
+    public  void add(int a, int b){
+        int sum =a+b;
+        return sum;
+    }
+}
