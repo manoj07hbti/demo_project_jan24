eeb9071f75891f9d529db293813a0d205ff94f12
Manoj Kumar <manojkumar.pcf@gmail.com>
2024-03-13 21:28:37 +0530

    Demo Scanner with user input

--- src/scanner_with_input/CalculatorApp.java
+++ src/scanner_with_input/CalculatorApp.java
@@ -25,20 +25,52 @@ public class CalculatorApp {
     }
 
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
-        System.out.println("Please enter value of a: ");
-        int a=scanner.nextInt();
-        System.out.println("Please enter value of b: ");
-        int b=scanner.nextInt();
-
         CalculatorApp obj= new CalculatorApp();
-        int result= obj.add(a,b);
+        int result=0;
+        Scanner scanner= new Scanner(System.in);
 
-        System.out.println("Addition is : "+result);
+        String userInput="Y";
+        while(userInput.equals("Y")){
 
-        //TODO call all methods and print output in main method
 
-    }
+
+            System.out.println("Please enter value of a: ");
+            int a=scanner.nextInt();
+            System.out.println("Please enter value of b: ");
+            int b=scanner.nextInt();
+            //TODO we should have option to select which operation you want to perform abd based on user choice out put should come
+            System.out.println("Please enter 1: Add , 2 :sub, 3:multi and 4:Divide ");
+            //TODO it should repeatedly ask for choice and based on choice it should exit the program otherwise keep asking for choice of calculator
+            int choice=scanner.nextInt();
+
+            switch (choice){
+                case 1: result=obj.add(a,b);
+                    System.out.println("Addition is :"+result);
+                    break;
+                case 2: result=obj.sub(a,b);
+                    System.out.println("Subtraction is :"+result);
+                    break;
+                case 3: result=obj.multi(a,b);
+                    System.out.println("Multi is :"+result);
+                    break;
+                case 4: double divide=obj.divide(a,b);
+                    System.out.println("Divide is :"+divide);
+                    break;
+                default:
+                    System.out.println("Invalid choice entered...Please enter 1,2,3, or 4 only. ");
+            }
+
+
+            System.out.println("Please enter Y to continue or press any other to key for EXIT from App...");
+            Scanner scanner2= new Scanner(System.in);
+            userInput=scanner2.nextLine();
+
+        }
+
+        }
+
+
+
 
 
 }
