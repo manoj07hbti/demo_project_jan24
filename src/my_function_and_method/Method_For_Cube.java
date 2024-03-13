cf8d7e5a192ddea3ee78e6b7cba5e287e8aa35f7
Niranjan singh <ns201890@gmail.com>
2024-03-13 15:14:31 +0530

    Method_For_Cube

---
+++ src/my_function_and_method/Method_For_Cube.java
@@ -0,0 +1,15 @@
+package my_function_and_method;
+
+public class Method_For_Cube {
+    public void make_cube(){
+        int a=5;
+        int cube=a*a*a;
+        System.out.println(" cube of the given number:-"+cube);
+    }
+
+    public static void main(String[] args) {
+        Method_For_Cube obj=new Method_For_Cube();
+        obj.make_cube();
+
+    }
+}
