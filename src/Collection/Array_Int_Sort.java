186d1b682bda86cc8b171f2f13a2a98e0121d6d8
Prafful <158169672+Prafful-g@users.noreply.github.com>
2024-03-21 13:04:42 +0530

    Collection int,sort

--- src/Collection/Array_Int_Sort.java
+++ src/Collection/Array_Int_Sort.java
@@ -7,28 +7,39 @@ import java.util.Scanner;
 //TODO find out all the marks between 33 to 44
 public class Array_Int_Sort {
 
+
     public ArrayList<Integer> marks() {
         ArrayList<Integer> marks = new ArrayList<>();
 
+        String input = "y";
+
+        Scanner scan = new Scanner(System.in);
         marks.add(35);
         marks.add(49);
         marks.add(40);
         marks.add(72);
+
+
+        while (input.equals("y")) {
+
+            System.out.println("Enter student marks:");
+            marks.add(scan.nextInt());
+            System.out.println("y to add");
+            input = scan.next();
+        }
 
         return marks;
 
 
     }
 
-    public ArrayList<Integer> sort(ArrayList<Integer> marks){
+    public ArrayList<Integer> sort(ArrayList<Integer> marks) {
         ArrayList<Integer> InRange = new ArrayList<>();
 
-        for (Integer Ann : marks){
-            if (Ann >=33 && Ann <=44)
+        for (Integer Ann : marks) {
+            if (Ann >= 33 && Ann <= 44)
                 InRange.add(Ann);
         }
-
-
 
 
         return InRange;
@@ -38,12 +49,18 @@ public class Array_Int_Sort {
     public static void main(String[] args) {
         Array_Int_Sort obj = new Array_Int_Sort();
 
-        ArrayList<Integer> S = obj.marks();
-        for (Integer var : S)
+        ArrayList<Integer> marks = obj.marks();
+        for (Integer var : marks)
             System.out.println(var);
 
 
+        System.out.println("***********************");
 
+
+        ArrayList<Integer> InRange = obj.sort(marks);
+        for (Integer d : InRange) {
+            System.out.println(d);
+        }
 
 
     }
