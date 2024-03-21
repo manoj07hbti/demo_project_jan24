53c6778ac0431594f454cea951aeef7dcb2dd09b
Prafful <158169672+Prafful-g@users.noreply.github.com>
2024-03-21 14:19:19 +0530

    Collection start with A

--- src/Collection/Collection_ArrayList_withInput.java
+++ src/Collection/Collection_ArrayList_withInput.java
@@ -7,15 +7,13 @@ public class Collection_ArrayList_withInput {
 
 
     // TODO take input from user and add to your list, ask user to continue or exit
+
+
     public ArrayList<String> createStringArrayList() {
         ArrayList<String> studentNames = new ArrayList<>();
         Scanner scan = new Scanner(System.in);
         String input = "y";
 
-        studentNames.add("ram");
-        studentNames.add("sita");
-        studentNames.add("ravan");
-        studentNames.add("hari");
 
 
         while (input.equals("y")) {
@@ -31,22 +29,36 @@ public class Collection_ArrayList_withInput {
     }
 
 
+    public ArrayList<String> sss(ArrayList<String> createStringArrayList2) {
 
+        ArrayList<String> Astart = new ArrayList<>();
+
+
+        for (String Ann : createStringArrayList2) {
+            if (Ann.toLowerCase().startsWith("a") || Ann.toUpperCase().startsWith("A")) {
+                Astart.add(Ann);
+            }
+
+        }
+
+
+        return Astart;
+    }
 
 
     public static void main(String[] args) {
         Collection_ArrayList_withInput obj = new Collection_ArrayList_withInput();
-        ArrayList<String> S = obj.createStringArrayList();
-        for (String s : S) {
+        ArrayList<String> Sort = obj.createStringArrayList();
+        for (String s : Sort) {
             System.out.println(s);
         }
+        System.out.println("\n"+"***********"+"\n");
 
-
-
-
-
-
-
+        ArrayList<String> A = obj.sss(Sort);
+        System.out.println("\n"+"Names starting with letter 'A':");
+        for (String name : A) {
+            System.out.println(name);
+        }
 
 
     }
