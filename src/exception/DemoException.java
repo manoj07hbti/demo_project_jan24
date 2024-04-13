dfd0940fab08767897311749b564c6287c8534b7
Manoj Kumar <manojkumar.pcf@gmail.com>
2024-04-13 21:41:32 +0530

    Exception and Exception Handling

---
+++ src/exception/DemoException.java
@@ -0,0 +1,27 @@
+package exception;
+
+public class DemoException {
+
+   public void divide(int a, int b){
+       System.out.println("Before Division....");
+       int result=0;
+       try {
+            result = a / b;
+       }
+       catch (Exception e){
+           System.out.println("Exception occurred ...."+e);
+       }
+       System.out.println("After Division....");
+       System.out.println("Division is : "+result);
+   }
+
+
+    public static void main(String[] args) {
+        DemoException obj= new DemoException();
+        obj.divide(4,2);
+        obj.divide(0,2);
+        obj.divide(4,0);
+
+    }
+
+}
